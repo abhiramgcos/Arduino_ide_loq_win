b2ed601d6f24500e7efdee61bb6fc79dc5c0f5fb
Abhiram G Nair <munnaabhiram321@gmail.com>
2026-04-15 11:47:50 +0530

    Refactor BLE connection logic and update scanning parameters for FT-1603A device

--- ble_force_connet/ble_force_connet.ino
+++ ble_force_connet/ble_force_connet.ino
@@ -1,23 +1,25 @@
 #include <NimBLEDevice.h>
 
 static NimBLEAddress targetAddr("13:26:aa:41:b7:d3", BLE_ADDR_RANDOM);
-static bool connected = false;
+static bool done = false;
 
-class ScanCallbacks : public NimBLEScanCallbacks {
-  void onResult(NimBLEAdvertisedDevice* d) override {
+class ScanCallbacks : public NimBLEAdvertisedDeviceCallbacks {
+  void onResult(NimBLEAdvertisedDevice* d) {  // NO override keyword
     if (d->getAddress() == targetAddr) {
       Serial.printf("Found FT-1603A! RSSI: %d | Connectable: %s\n",
         d->getRSSI(), d->isConnectable() ? "YES" : "NO");
 
-      if (d->isConnectable()) {
+      if (d->isConnectable() && !done) {
         NimBLEDevice::getScan()->stop();
         NimBLEClient* pClient = NimBLEDevice::createClient();
         if (pClient->connect(d)) {
           Serial.println("Connected! Deleting bond...");
-          NimBLEDevice::deleteBond(targetAddr);
+          NimBLEDevice::deleteBond(d->getAddress());
           pClient->disconnect();
-          Serial.println("Done. Power cycle keyboard now!");
-          connected = true;
+          Serial.println("Done! Power cycle the keyboard now.");
+          done = true;
+        } else {
+          Serial.println("Connect failed. Retrying...");
         }
       }
     }
@@ -28,15 +30,18 @@ void setup() {
   Serial.begin(115200);
   NimBLEDevice::init("ESP32Host");
   NimBLEDevice::setSecurityAuth(true, true, true);
+
   NimBLEScan* pScan = NimBLEDevice::getScan();
-  pScan->setScanCallbacks(new ScanCallbacks(), false);
+  pScan->setAdvertisedDeviceCallbacks(new ScanCallbacks()); // v1.x method
   pScan->setActiveScan(true);
+  pScan->setInterval(100);
+  pScan->setWindow(99);
 }
 
 void loop() {
-  if (!connected) {
-    Serial.println("Scanning for FT-1603A...");
-    NimBLEDevice::getScan()->start(3, false);
+  if (!done) {
+    Serial.println("Scanning...");
+    NimBLEDevice::getScan()->start(4, false);
     NimBLEDevice::getScan()->clearResults();
     delay(500);
   }
